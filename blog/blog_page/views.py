8e300a8af178f5daedc20a04c33167c011f57854
kekeli-the-light <kekelidompeh@gmail.com>
2023-08-25 14:12:12 +0000

    looked here

--- blog/blog_page/views.py
+++ blog/blog_page/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Post
-
-# Create your views here.
 
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, "blog_page/home.html", context)
-
 def about(request):
     return render(request, "blog_page/about.html", {'title': 'About'})
 
