4772a23eef4c94f2ac0b5c62703b8e672851662a
kekeli-the-light <kekelidompeh@gmail.com>
2023-08-26 13:50:53 +0000

    some changes

--- blog/blog_page/views.py
+++ blog/blog_page/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .models import Post
 
+
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, "blog_page/home.html", context)
+
+
 def about(request):
     return render(request, "blog_page/about.html", {'title': 'About'})
-
--- blog/users/views.py
+++ blog/users/views.py
@@ -3,17 +3,19 @@ from django.contrib import messages
 from .forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required
 
+
 def register(request):
     if request.method =='POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Accont Created! You can Login Now!')
+            messages.success(request, f'Account Created! You can Login Now!')
             return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})     
+
 
 @login_required
 def profile(request):
