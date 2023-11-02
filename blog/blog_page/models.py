ca46e078f13f455c4809790a53b8af714d785e5d
kekeli-the-light <kekelidompeh@gmail.com>
2023-11-02 13:51:37 +0000

    updated

--- blog/blog_page/models.py
+++ blog/blog_page/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 from django.contrib.auth.models import User
-
+from taggit.managers import TaggableManager
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
